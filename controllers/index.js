82c6d1f52f52637519557c7faff60a4a04e7a15e
Brittany Brimley <brittanysapp@gmail.com>
2023-10-13 02:47:17 -0600

    completed homeRoutes and controllers index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
-// const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./api/blogRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/blog', blogRoutes);
+router.use('/blog', blogRoutes);
 
 module.exports = router;
