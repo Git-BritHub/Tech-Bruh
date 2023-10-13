9c88e64e8e1b2cf9e538a334e83c857b3df65a85
Brittany Brimley <brittanysapp@gmail.com>
2023-10-13 03:17:01 -0600

    deleted unnecessary imports for server.js page

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const bcrypt = require('bcrypt');
-const mysql = require('mysql2');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
