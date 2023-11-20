25f9a1445f61a66ac869ce03e4062abaa1fec60a
Brittany Brimley <brittanysapp@gmail.com>
2023-11-20 15:15:32 -0700

    deleted 'logging: false' to address access denied error message for user

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: process.env.DB_HOST,
       dialect: 'mysql',
-      logging: false,
       port: process.env.DB_PORT
     });
 
