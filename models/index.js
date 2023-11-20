5d4d1b0deba0489df0d3f52b7cc87c4ff6403d01
Brittany Brimley <brittanysapp@gmail.com>
2023-11-20 16:20:34 -0700

    updated index.js model to include Post and Comment instead of Blog

--- models/index.js
+++ models/index.js
@@ -1,13 +1,38 @@
 const User = require('./User');
-const Blog = require('./Blog');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+// User Post belongs to User
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// User has many Posts
 User.hasMany(Blog, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
-Blog.belongsTo(User, {
-  foreignKey: 'user_id'
+// User Comment belongs to User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Blog };
+// User has many Comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// User Comment belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+// User Post has many Comments
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+module.exports = { User, Post, Comment };
