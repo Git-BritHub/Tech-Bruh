1b1f353f080103f529a2781fa1eede4555233611
Brittany Brimley <brittanysapp@gmail.com>
2023-11-20 17:33:23 -0700

    deleted old/unnecessary blogRouting

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
-const blogRoutes = require('./api/postRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/blog', blogRoutes);
 
 module.exports = router;
